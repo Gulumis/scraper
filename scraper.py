d7b84d826c73f7e7164084272cdd6e1cd5016ddd
gulumis <a.gouloumis@alumnos.upm.es>
2020-04-10 14:20:20 +0200

    Scrapped news of 'OKDiario' day 2020-04-10 14:00

--- scraper.py
+++ scraper.py
@@ -1,3 +1,21 @@
-import requests
-from bs4 import BeautifulSoup
+from urllib.request import Request, urlopen
+from bs4 import BeautifulSoup as soup
+import pandas as pd 
 
+url = 'https://okdiario.com/'
+req = Request(url , headers={'User-Agent': 'Mozilla/5.0'})
+
+webpage = urlopen(req).read()
+page_soup = soup(webpage, "html.parser")
+title = page_soup.find("title")
+
+list = []
+df = pd.DataFrame()
+
+containers = page_soup.findAll("p","article-lead", text=True)
+for container in containers:
+    list.append(container.find(text=True).strip())
+
+df = list
+
+print(df)
