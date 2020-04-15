cf41c7739d18ca1b0a49b453fd97941d8c644565
gulumis <a.gouloumis@alumnos.upm.es>
2020-04-15 21:20:09 +0200

    Modify main()

--- scraper.py
+++ scraper.py
@@ -2,20 +2,25 @@ from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup as soup
 import pandas as pd 
 
-url = 'https://okdiario.com/'
-req = Request(url , headers={'User-Agent': 'Mozilla/5.0'})
+def main():
 
-webpage = urlopen(req).read()
-page_soup = soup(webpage, "html.parser")
-title = page_soup.find("title")
+    url = 'https://okdiario.com/'
+    req = Request(url , headers={'User-Agent': 'Mozilla/5.0'})
 
-list = []
-df = pd.DataFrame()
+    webpage = urlopen(req).read()
+    page_soup = soup(webpage, "html.parser")
+    title = page_soup.find("title")
 
-containers = page_soup.findAll("p","article-lead", text=True)
-for container in containers:
-    list.append(container.find(text=True).strip())
+    list = []
+    df = pd.DataFrame()
 
-df = list
+    containers = page_soup.findAll("p","article-lead", text=True)
+    for container in containers:
+        list.append(container.find(text=True).strip())
 
-print(df)
+    df = list
+    return df
+
+if __name__ == "__main__":
+    df = main()
+    print(df)
